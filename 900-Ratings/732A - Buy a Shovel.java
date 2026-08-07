096c65639478fde8eeb9bc7901b8ddee2f1f2ca5
dikshamitra <dikshamitra3109@gmail.com>
2026-08-07 18:22:48 +0530

    Add today's Codeforces Java solution

---
+++ 900-Ratings/732A - Buy a Shovel.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int k = sc.nextInt();
+        int r = sc.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            int total = k * i;
+            if (total % 10 == 0 || total % 10 == r) {
+                System.out.println(i);
+                break;
+            }
+        }
+    }
+}
