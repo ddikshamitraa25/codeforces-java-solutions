2feda43308f6394d33ebd9497fac2d641a70300d
dikshamitra <dikshamitra3109@gmail.com>
2026-08-29 10:25:06 +0530

    Add today's Codeforces Java solution

---
+++ 1100-Ratings/318A - Even Odds.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        long k = sc.nextLong();
+        long odds = (n + 1) / 2;
+        if (k <= odds) {
+            System.out.println(2 * k - 1);
+        } else {
+            System.out.println(2 * (k - odds));
+        }
+    }
+}
